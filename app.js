69c15d3682272cc0e349a72e10b8d7485b061825
Leon <tutmr999@gmail.com>
2023-03-19 13:40:41 +0900

    add chat.js file and code

--- app.js
+++ app.js
@@ -12,11 +12,15 @@ const io = socketIO(server);
 app.use(express.static(path.join(__dirname, "src")))
 
 //포트는 프로세스 환경에서 포 설정된 값으로 할당, 없을 경우 5001번
-const PORT = process.env.PORT || 5001; 
+const PORT = process.env.PORT || 5500; 
 server.listen(PORT, () => console.log(`server is running ${PORT}`));
 
 
 
 io.on('connection', (socekt) => {
-    console.log("Success connection");
+    // console.log("연결")
+    socekt.on("chatting", (data) => {
+        console.log(data);
+        io.emit('chatting', `Hi ${data}`);
+    });
 });
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+'use strict'
+const socket = io();
+
+// 메세지 보내기
+socket.emit('chatting', "from front");
+
+// 메세지 받기
+socket.on('chatting', (data) => {
+    console.log(data);
+});
+
+console.log(socket);
