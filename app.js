0b2f9ec891dd734c14aca5f04e3fa55378ca0ebc
Leon <tutmr999@gmail.com>
2023-03-19 13:28:41 +0900

    add js file

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const http = require('http');
 const app = express();
 const path = require('path');
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+
+const io = socketIO(server);
 
 
-
+// index 읽기
 app.use(express.static(path.join(__dirname, "src")))
 
-//포트는 프로세스 환경에서 포 설정된 값으로 할당, 없을 경우 5000번
+//포트는 프로세스 환경에서 포 설정된 값으로 할당, 없을 경우 5001번
 const PORT = process.env.PORT || 5001; 
+server.listen(PORT, () => console.log(`server is running ${PORT}`));
 
-app.listen(PORT, () => console.log(`server is running ${PORT}`));
+
+
+io.on('connection', (socekt) => {
+    console.log("Success connection");
+});
