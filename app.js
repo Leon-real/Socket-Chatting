8948a6435dcd1d661c7f9472920c3674258d3d31
Leon <tutmr999@gmail.com>
2023-03-19 13:15:25 +0900

    add nodemon file

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+
+
+app.use(express.static(path.join(__dirname, "src")))
+
+//포트는 프로세스 환경에서 포 설정된 값으로 할당, 없을 경우 5000번
+const PORT = process.env.PORT || 5001; 
+
+app.listen(PORT, () => console.log(`server is running ${PORT}`));
